a3afe2ab9639e221e00a06eaa58f9409ca3f5f4b
john <jmsalvador2359@gmail.com>
2021-04-09 14:36:26 -0500

    changed to use randn instead of generate random numbers

--- neural_net/neural_net.py
+++ neural_net/neural_net.py
@@ -30,19 +30,24 @@ def data_to_array(fname):
 	return trng_labels, trng_set
 
 class TwoLayerNN:
-	def __init__(self, num_inputs, num_outputs):
+	def __init__(self, num_inputs=1, num_outputs=1):
+		#instantiate number of inputs
+		self.num_inputs=num_inputs
+
+		#instanstiate number of outputs
+		self.num_outputs=num_outputs
 
 		#100 hidden units for input layer
 		self.h_size=100
 
 		#randomly initialize hidden layer weights
-		self.h_matrix=np.random.randint(-500, 500, (self.h_size, num_inputs+1)) #+1 on num_inputs for the bias
+		self.h_matrix=np.random.randn(self.h_size, num_inputs+1) #+1 on num_inputs for the bias
 
 		#set bias weight to 1 for input weight vectors
 		self.h_matrix[:,0]=1
 
 		#randomly initialize output layer weights
-		self.v_matrix=np.random.randint(-500, 500, (num_outputs, self.h_size+1)) #+1 on h_size to account for bias
+		self.v_matrix=np.random.randn(num_outputs, self.h_size+1) #+1 on h_size to account for bias
 
 		#set bias weight to 1 for output weight vectors
 		self.v_matrix[:,0]=1
@@ -54,14 +59,14 @@ class TwoLayerNN:
 
 
 	def forward_propogation(self, data):
-
+		data=np.insert(data, 0, 1.0, axis=0)
 		#compute dot prodcuts for data with all hidden layer vectors into hidden_output
-		hidden_output=np.array(self.h_size+1)
+		hidden_output=np.zeros(self.h_size+1)
 		for i in range(self.h_matrix.shape[0]):
 			hidden_output[i]=np.dot(self.h_matrix[i], data)
 
 		#comput dot products for hidden_output with output layer vectors into output
-		output=np.array(num_outputs)
+		output=np.zeros(self.num_outputs)
 		for i in range(self.v_matrix.shape[0]):
 			output[i]=np.dot(self.v_matrix[i], hidden_output)
 
